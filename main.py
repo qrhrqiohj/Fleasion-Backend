20694571668837d3bb7da72f20dce615f926d008
Gruber450 <68507886+qrhrqiohj@users.noreply.github.com>
2025-12-29 00:49:09 +0000

    Update main.py

--- main.py
+++ main.py
@@ -539,6 +539,7 @@ if __name__ == "__main__":
     print(f'>> {Fore.MAGENTA}FLEASION IS CURRENTLY DEPRECATED, A NEW AND IMPROVED VERSION IS BEING WORKED ON discord.gg/hXyhKehEZF{Style.RESET_ALL} <<')
     #print(f'>> {Fore.MAGENTA}MOST CHANGES MADE WITH ROBLOX OPEN WILL NOT APPLY UNTIL REJOIN{Style.RESET_ALL} <<\n\nFor news and more resources, check out our discord server!\ndiscord.gg/hXyhKehEZF')
     while True:
+        break
         game_pre = ""
         run_option = get_valid_input(f"\nWelcome to: Fleasion!\n"
                            f"0: {Fore.GREEN}Custom{Style.RESET_ALL}\n"
@@ -553,7 +554,7 @@ if __name__ == "__main__":
                            f"Enter which option you want to select.\n: ",
                            valid_values=[0, 1, 2, 3, 4, 5, 6, 7, 8],
                            top = True
-        #)
+        )
         try:
             match run_option:
                 case 0:
@@ -1131,6 +1132,7 @@ Enter to return: """
                     )
         except Exception as e:
             print(f"{Fore.RED}An error occurred: {e}{Style.RESET_ALL}")
+
 
 
 
